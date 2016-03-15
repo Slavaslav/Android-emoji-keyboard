734ff1374b9de57f2923de36ce049551b6f22546
awecea <awecea@gmail.com>
2016-03-15 12:46:46 +0200

    add

--- app/src/main/java/com/slava/emojicfc/Emoji.java
+++ app/src/main/java/com/slava/emojicfc/Emoji.java
@@ -7,10 +7,19 @@ public class Emoji {
     public static HashMap<CharSequence, Integer> hashMap = new HashMap<>();
 
     static {
+
+        String resourceName;
+        int n = 1;
+
         for (int i = 0; i < EmojiData.emojiData.length; i++) {
-            for (int k = 0; k < EmojiData.emojiData[i].length; k++)
-                hashMap.put(EmojiData.emojiData[i][k], R.drawable.emoji_1f604);
+            for (int k = 0; k < EmojiData.emojiData[i].length; k++) {
+                resourceName = "emoji_" + n;
+                int resourceID = MainActivity.applicationContext.getResources().getIdentifier(resourceName, "drawable",
+                        MainActivity.applicationContext.getPackageName());
+
+                hashMap.put(EmojiData.emojiData[i][k], resourceID);
+                n++;
+            }
         }
     }
-
 }
